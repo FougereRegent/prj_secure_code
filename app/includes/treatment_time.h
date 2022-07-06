d79ff647c43802bf25858a082a5f2e70a89f6c9a
Damien <damien.venant@outlook.com>
2022-07-06 15:30:56 +0200

    Commencement de la partie de développement du retour de l'heure

---
+++ app/includes/treatment_time.h
@@ -0,0 +1,10 @@
+//
+// Created by damien on 7/6/22.
+//
+
+#ifndef PRJ_TREATMENT_TIME_H
+#define PRJ_TREATMENT_TIME_H
+#define SUCCESS_FORMAT 0
+#define FAILED_FORMAT -1
+extern int get_time(char *format_time, char* out_result);
+#endif //PRJ_TREATMENT_TIME_H
---
+++ app/src/treatment_time.c
@@ -0,0 +1,8 @@
+#include <time.h>
+#include "treatment_time.h"
+
+/*Not implemented*/
+extern int get_time(char* format_time, char* out_result)
+{
+    return SUCCESS_FORMAT;
+}
