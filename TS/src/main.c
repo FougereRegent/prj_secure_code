7e3a19813e9175049ffa02b65d2875e0bb4e46c2
Damien <damien.venant@outlook.com>
2022-07-21 11:04:39 +0200

    Time set finished there are secure function that are implemented

--- TS/src/main.c
+++ TS/src/main.c
@@ -14,9 +14,9 @@ int check_format_time(const char* format_time);
 
 int main(int argc, char **argv)
 {
-    if(argc == 2)
+    if(argc == 3)
     {
-        const size_t size_string_date_time_format = strlen(argv[1]) + 1;
+        const size_t size_string_date_time_format = strlen(argv[1]) + strlen(argv[2]) + 2;
         char *date_time_string = (char*) calloc(sizeof(char), size_string_date_time_format);
 
         if(date_time_string == NULL)
@@ -25,10 +25,11 @@ int main(int argc, char **argv)
                 return EXIT_FAILURE;
         }
 
-        strncpy(date_time_string, argv[1], size_string_date_time_format);
-        if(set_time(date_time_string) < -1)
+        snprintf(date_time_string, size_string_date_time_format, "%s %s", argv[1], argv[2]);
+        if(set_time(date_time_string) < 0)
         {
             free(date_time_string);
+            printf("Error\n");
             return EXIT_FAILURE;
         }
         free(date_time_string);
@@ -42,7 +43,7 @@ int check_format_time(const char *format_date)
 
     if(regcomp(&regex_check_format_time, "^([0-1][0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$", REG_EXTENDED) != 0)
     {
-        printf("The regualr expression doesn't compile\n");
+        printf("The regular expression doesn't compile\n");
         return -1;
     }
 
@@ -55,21 +56,19 @@ int check_format_time(const char *format_date)
 
 int set_time(const char *format_time)
 {
+    /*
     if(check_format_time(format_time) < 0) {
         printf("Format error, use this format hh:mm:ss\n");
         return -1;
     }
+    */
+    time_t t = time(NULL);
+    struct tm *time;
 
-    const char hours[2] = {format_time[0], format_time[1]};
-    const char minutes[2] = {format_time[3], format_time[4]};
-    const char seconds[2] = {format_time[6], format_time[7]};
-
-    time_t raw_time = time(NULL);
-    struct tm *time  = localtime(&raw_time);
-
-    time->tm_hour = atoi(hours);
-    time->tm_min = atoi(minutes);
-    time->tm_sec = atoi(seconds);
+    if(strptime(format_time, "%d/%m/%Y %H:%M:%S", time) == NULL)
+    {
+        return -1;
+    }
 
     struct timeval new_time = { mktime(time), 0 };
 
